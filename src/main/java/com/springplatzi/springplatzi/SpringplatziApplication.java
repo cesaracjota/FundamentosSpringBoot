56bcc598a5619bcf56388e8642e3ac0a10555deb
Cesar Acjota Merma <cesar.acjota@tecsup.edu.pe>
2022-04-18 22:05:15 -0500

    fist commit

---
+++ src/main/java/com/springplatzi/springplatzi/SpringplatziApplication.java
@@ -0,0 +1,13 @@
+package com.springplatzi.springplatzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringplatziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringplatziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springplatzi/springplatzi/SpringplatziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springplatzi.springplatzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringplatziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
